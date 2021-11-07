2245b712da62f33b538a02ef8aa310a7c4d4245c
SakshiShreya <shreyasakshi96@gmail.com>
2021-11-07 23:30:16 +0530

    feat: Setup mongo

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
+// CONNECT MONGO
+const DB = process.env.DB.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD,
+).replace("<USERNAME>", process.env.DB_USERNAME);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log("DB connection successfull"));
+
+// CONNECT EXPRESS
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
